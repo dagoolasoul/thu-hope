d88175075da977a7b6e1635439a7d8226f6e09ab
dagoolasoul <46871308+dagoolasoul@users.noreply.github.com>
2023-08-31 00:26:45 +0800

    Adjust font size.

--- tree.js
+++ tree.js
@@ -109,7 +109,7 @@ var Tree = function() {
 		//Add label
 		if(this.label != ''){
 	
-			var label = new SpriteText(this.label /*+'['+this.id+']'*/, panorama_mode ? .8 : .6);
+			var label = new SpriteText(this.label /*+'['+this.id+']'*/, panorama_mode ? .7 : .6);
 			label.fontFace = 'Noto Sans TC';
 			label.fontWeight = 'bold';
 			label.fontSize = 270;
@@ -221,7 +221,7 @@ var Tree = function() {
 			}
 			
 			if(b.label && b.label != ''){
-				var label = new SpriteText( b.label/*+'['+b.id+']'*/, panorama_mode ? .5 : .3);
+				var label = new SpriteText( b.label/*+'['+b.id+']'*/, panorama_mode ? .45 : .3);
 				label.fontFace = 'Noto Sans TC'; //'微軟正黑體';
 				label.fontWeight = 'bold';
 				label.fontSize = 270;
@@ -582,7 +582,7 @@ var Leave = function() {
 		//Add tag label
 		if(showTags && this.label != ''){
 
-			this.labelSprite = new SpriteText(this.label, panorama_mode ? .44 : .24);
+			this.labelSprite = new SpriteText(this.label, panorama_mode ? .4 : .24);
 			this.labelSprite.position.x = this.x;
 			this.labelSprite.position.y = this.y - 1;
 			this.labelSprite.position.z = this.z;
